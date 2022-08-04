1f38b8398c4d5d888dfbe458f44e8c8e1db36d53
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-08-04 14:57:26 -0300

    Add files via upload

---
+++ c34/sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var rope,fruit,ground;
+var fruit_con;
+
+
+
+
+
+function setup() 
+{
+  createCanvas(600,700);
+  frameRate(80);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  
+  rope = new Rope(7,{x:300,y:90});
+  
+
+
+  ground = new Ground(300,height,width,20);
+ 
+  
+  fruit = Bodies.circle(300,300,20);
+  Matter.Composite.add(rope.body,fruit);
+
+  fruit_con = new Link(rope,fruit);
+ 
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  background(51);
+
+
+  
+  ellipse(fruit.position.x,fruit.position.y,20,20);
+  
+  rope.show();
+ 
+
+  Engine.update(engine);
+  ground.show();
+
+  
+}
+
+
+
+
